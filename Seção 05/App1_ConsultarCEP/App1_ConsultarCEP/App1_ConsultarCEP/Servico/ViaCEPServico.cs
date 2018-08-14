f3c094a19b1f29359ec6d1634e45e5ce1bff22eb
Vagner H <vagner_h3rd7@outlook.com>
2018-08-14 16:29:50 -0300

    App01 - Cooncluído

---
+++ Seção 05/App1_ConsultarCEP/App1_ConsultarCEP/App1_ConsultarCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App1_ConsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App1_ConsultarCEP.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string Contedo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Contedo);
+
+            if (end.cep == null) return null;
+
+            return end;
+        }
+    }
+}
